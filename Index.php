8cbdff6eec585894e086670bafc4b059b9ce3345
4st4C <152559925+4st4C@users.noreply.github.com>
2023-11-30 20:06:01 -0300

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8" />
+    <title>WIE UEM</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link href="./assets/img/brand/favicon.svg" rel="icon" type="image/png">
+    <link rel="stylesheet" type="text/css" href="style-wie.css">
+</head>
+<header>
+    <div class="navegation">
+        <ul>
+            <li>
+                <a href="#">
+                    <span class="icon"><ion-icon name="home"></ion-icon></span>
+                    <span class="text">Principal</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <span class="icon"><ion-icon name="chatbox"></ion-icon></span>
+                    <span class="text">Sobre nós</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <span class="icon"><ion-icon name="trail-sign"></ion-icon></span>
+                    <span class="text">Eventos</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <span class="icon"><ion-icon name="people"></ion-icon></span>
+                    <span class="text">Nossa Equipe</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+</header>
+<body>
+
+
+
+
+
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</body>
+</html>
