c6edc515211a1c53025e7645f8b68e88ee6c06fb
4st4C <152559925+4st4C@users.noreply.github.com>
2023-12-01 09:29:28 -0300

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,3 +1,4 @@
+<?php
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -5,45 +6,217 @@
     <title>WIE UEM</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <link href="./assets/img/brand/favicon.svg" rel="icon" type="image/png">
-    <link rel="stylesheet" type="text/css" href="style-wie.css">
+    <link rel="stylesheet" type="text/css" href="Style-Index.css">
 </head>
 <header>
-    <div class="navegation">
+    <span class="nome">IEEE WIE UEM</span>
+    <div class="navigation">
         <ul>
-            <li>
-                <a href="#">
+            <li class="list active">
+                <a href="#home">
                     <span class="icon"><ion-icon name="home"></ion-icon></span>
                     <span class="text">Principal</span>
                 </a>
             </li>
-            <li>
-                <a href="#">
+            <li class="list">
+                <a href="#about">
                     <span class="icon"><ion-icon name="chatbox"></ion-icon></span>
                     <span class="text">Sobre nós</span>
                 </a>
             </li>
-            <li>
-                <a href="#">
+            <li class="list">
+                <a href="#event">
                     <span class="icon"><ion-icon name="trail-sign"></ion-icon></span>
                     <span class="text">Eventos</span>
                 </a>
             </li>
-            <li>
-                <a href="#">
+            <li class="list">
+                <a href="#teams">
                     <span class="icon"><ion-icon name="people"></ion-icon></span>
                     <span class="text">Nossa Equipe</span>
                 </a>
             </li>
+            <div class="indicator"></div>
         </ul>
     </div>
 </header>
 <body>
+<section id="home">
+    <div class="main">
+        <img class="background" src="img/background.png" alt="background">
+    </div>
+    <h1 class="title">IEEE - Woman in Engineering</h1>
+    <div class="main_box"> 
+        <span class="main_text">
+            <span class="line">Aqui vai o texto principal sobre o WIE</span>
+            <span class="line">Aqui vai o texto principal sobre o WIE</span>
+            <span class="line">Aqui vai o texto principal sobre o WIE</span>
+            <span class="line">Aqui vai o texto principal sobre o WIE</span>
+            <!-- Adicione mais linhas conforme necessário -->
+        </span>
+    </div>
+</section>
+<section id="about">
+    <div class="about_us">
+        <h2>SOBRE NÓS</h2>
+        <span> 
+            Aqui vai o texto falando melhor sobre o WIE na UEM, sobre a nossa história, pontos relevantes e como está hoje
+            Aqui vai o texto falando melhor sobre o WIE na UEM, sobre a nossa história, pontos relevantes e como está hoje
+            Aqui vai o texto falando melhor sobre o WIE na UEM, sobre a nossa história, pontos relevantes e como está hoje
+            Aqui vai o texto falando melhor sobre o WIE na UEM, sobre a nossa história, pontos relevantes e como está hoje
+            Aqui vai o texto falando melhor sobre o WIE na UEM, sobre a nossa história, pontos relevantes e como está hoje
 
+        </span>
+    </div>
 
+    <div class="objective">
+        <h2>OBJETIVO</h2>
+        <span> 
+            Aqui vai o texto falando melhor sobre os principais objetivos do WIE 
+            Aqui vai o texto falando melhor sobre os principais objetivos do WIE 
+            Aqui vai o texto falando melhor sobre os principais objetivos do WIE 
+            Aqui vai o texto falando melhor sobre os principais objetivos do WIE 
+            Aqui vai o texto falando melhor sobre os principais objetivos do WIE 
+        </span>
+    </div>
 
+    <div class="join_us">
+        <h2>JUNTE-SE</h2>
+        <span> 
+            Siga a nossa conta principal no instagram, além de sempre estarmos realizando publicações de projetos, campanhas e indicações, 
+            também anunciamos com antecedência qual a data do nosso processo seletivo e as informações necessárias para participar,
+            venha fazer parte da nossa equipe!
+        </span>
+    </div>
+    <div class="join">
+        <a href="https://www.instagram.com/wie_ieeeuem/" target="_blank">JOIN US</a>
+    </div>
+</section>
+<section id="event">
+    <div class="events">
+        <h2>EVENTOS</h2>
+        
+        <div class="events_box">
+            <!-- Coluna 1 -->
+            <div class="box">
+                <a href="https://www.instagram.com/p/CxYJHOvr9ee/" target="_blank">
+                    <img src="img/event1.png" alt="Imagem 1">
+                    <p>Oficina de Sorvete</p>
+                </a>
+            </div>
 
+            <!-- Coluna 2 -->
+            <div class="box">
+                <a href="https://www.instagram.com/p/CyQutzMgPpd/" target="_blank">
+                    <img src="img/event2.png" alt="Imagem 2">
+                    <p>DIA 18 - USE ROSA</p>
+                </a>
+            </div>
 
+            <!-- Coluna 3 -->
+            <div class="box">
+                <a href="https://www.instagram.com/p/C0MMue6Ayqm/?img_index=1" target="_blank">
+                    <img src="img/event3.png" alt="Imagem 3">
+                    <p>WIE pergunta, vocês respondem</p>
+                </a>
+            </div> 
+        </div>    
+    </div>
+</section>
+<section id="teams">
+    <div class="team">
+        <h2>DIRETORIA</h2>
+        <div class="inline">
+            <div class="colunm">
+                <img src="img/presidente.png" alt="A PRESIDAS BD">
+                <span>Presidente</span>
+                <span>Maria Carolina</span>
+            </div>
+            <div class="colunm">
+                <img src="imagem3.jpg" alt="Imagem 3">
+                <span>Presidente</span>
+                <span>Maria Carolina</span>
+            </div>
+            <div class="colunm">
+                <img src="imagem3.jpg" alt="Imagem 3">
+                <span>Presidente</span>
+                <span>Maria Carolina</span>
+            </div>
+        </div>
+        <div class="allgroup">
+            <img src="img/allgroup.jpg" alt="Imagem do Grupo">
+        </div>
+    </div>
+</section>
+    <footer class="footer">
+        <div class="footer-container">
+          <div class="footer-row">
+            <div class="footer-col">
+              <h4>Links</h4>
+              <ul>
+                <li><a href="http://www.uem.br/" target="_blank">UEM Official Website</a></li>
+                <li><a href="https://www.ieee.org/about/index.html" target="_blank">IEEE Official Website</a></li>
+                <li><a href="https://wie.ieee.org/" target="_blank">IEEE WIE Official Website</a></li>
+                <li><a href="https://site.ieee.org/r1-sac/resources/student-scholarships/" target="_blank">IEEE Scholarships</a></li>
+                <li><a href="https://www.ieee.org/membership/benefits/" target="_blank">IEEE Benefits</a></li>
+                <li><a href="https://wie.ieee.org/membership/">IEEE WIE Membership</li>
+                <li><a href="https://www.ieee.org/membership/join/index.html" target="_blank">IEEE Global Membership</a></li>
+              </ul>
+            </div>
+            <div class="footer-col">
+              <h4>Contact Us</h4>
+              <ul>
+                <li class="space">
+                  +91 91110 08100<br>
+                  <span class="name">Maria Carolina</span>
+                </li>
+                <li>
+                  +91 90990 20893<br>
+                  <span class="name">Vice Presidente</span>
+                </li>
+              </ul>
+            </div>
+            <div class="footer-col">
+              <h4>Follow Us</h4>
+              <div class="social-links">
+                <a href="https://www.instagram.com/wie_ieeeuem/" target="_blank"><ion-icon class="icone" name="logo-instagram"></ion-icon></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+
+    <script>
+        const list = document.querySelectorAll('.list');
+        function activeLink(){
+            list.forEach((item) => item.classList.remove('active'));
+            this.classList.add('active');
+        }
+        list.forEach((item) => item.addEventListener('click',activeLink))
+    </script>
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+            const links = document.querySelectorAll('.list a');
+
+            function scrollToSection(event) {
+                event.preventDefault();
+
+                const targetId = this.getAttribute('href').substring(1);
+                const targetSection = document.getElementById(targetId);
+
+                if (targetSection) {
+                    targetSection.scrollIntoView({
+                        behavior: 'smooth',
+                        block: 'start'
+                    });
+                }
+            }
+
+            links.forEach(link => link.addEventListener('click', scrollToSection));
+        });
+    </script>    
 </body>
 </html>
+?>
